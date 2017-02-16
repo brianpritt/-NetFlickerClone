ee5d2a8fa0e65821c630e34b97fac302a64b3be8
Brian Pritt and Ryan Mathisen <brian+ryan@>
2017-02-16 15:16:41 -0800

    add unit testing

---
+++ Tests/ControllerTests/PhotoTest.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+using FlickrClone.Models;
+
+namespace Tests.ControllerTests
+{
+    public class PhotoTest
+    {
+        public void GetTitleTest()
+        {
+            var photo = new Photo("Name", "Description", null , "9d55808f-f5b8-4495-88eb-1ad0073bd20e");
+            var result = photo.PhotoName;
+            Assert.Equal(result, "Name");
+        }
+    }
+}
---
+++ Tests/ControllerTests/PhotosControllerTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using FlickrClone.Controllers;
+using FlickrClone.Models;
+using Xunit;
+
+namespace Tests.ControllerTests
+{
+    public class PhotosControllerTest
+    {
+        [Fact]
+        public void GetModel_List_Index_Test()
+        {
+            HomeController controller = new HomeController();
+            IActionResult actionResult = controller.Index();
+            ViewResult indexView = controller.Index() as ViewResult;
+
+            var result = indexView.ViewData.Model;
+
+            Assert.IsType<List<Photo>>(result);
+         }
+
+        [Fact]
+        public void Account_Index_Test()
+        {
+            HomeController controller = new HomeController();
+
+            var result = controller.Create(null, "Name", "Description", "9d55808f-f5b8-4495-88eb-1ad0073bd20e");
+
+            Assert.IsType<RedirectToActionResult>(result);
+        }
+
+    }
+}
